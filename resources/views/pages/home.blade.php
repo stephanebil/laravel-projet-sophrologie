a6e8f50fd475e752c8f4cd23eacdb0a110e0d5b0
stephanebil <stephanebilella@yahoo.fr>
2022-12-05 13:12:05 +0100

    laravel-projet-sophrologie amélioration 4 ok

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.main-layout title="Bienvenue à vous" >
     @include('partials.hero._hero')
-    @include('partials.accueil._part1-intro')
-    @include('partials.accueil._part2-propositions')
-    @include('partials.accueil._part3-confiance')
+    @include('partials.home._part1-intro')
+    @include('partials.home._part2-propositions')
+    @include('partials.home._part3-confiance')
     
     
 </x-layouts.main-layout>
